94a0b6fca271ef3563e4fa431e1239f71bd9a23f
LaelRodrigues <laelrodrigues7@gmail.com>
2017-11-22 19:21:05 -0300

    Adicionando os arquivos quadrado.h e quadrado.cpp

---
+++ include/quadrado.h
@@ -0,0 +1,59 @@
+/** 
+ * @file	quadrado.h
+ * @brief	Definicao da classe Quadrado para representar
+ *			um quadrado
+ * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
+ * @since	22/11/2017
+ * @date 	22/11/2017
+ */	
+
+#ifndef QUADRADO_H
+#define QUADRADO_H
+
+#include <ostream>
+using std::ostream;
+
+#include "plana.h"
+#include "figura.h"
+
+
+namespace geometria {
+	
+	/**
+ 	 * @class 	Quadrado quadrado.h
+	 * @brief	Classe que representa um quadrado
+ 	 * @details O atribulo do quadrado e o seu lado
+ 	 */
+	class Quadrado : public Plana {
+		
+		private:		
+			float lado;				/**< Lado do quadrado */
+
+		public:
+
+			/** @brief Construtor padrao */
+			Quadrado();
+
+			/** @brief Construtor parametrizado */	
+			Quadrado(float _lado);
+
+			/** @brief Retorna o lado do quadrado */
+			float getLado();
+
+			/** @brief Modifica o lado do quadrado */
+			void setLado(float _lado);
+
+			/** @brief Calcular a area do quadrado */
+			float calcArea(); 
+		
+			/** @brief Retorna o perimetro do quadrado */
+			float calcPerimetro();
+			
+		private:
+
+			/** @brief Metodo para imprimir os dados de um quadrado */
+			std::ostream& print(std::ostream &o) const;
+	};
+}
+
+#endif
---
+++ src/quadrado.cpp
@@ -0,0 +1,65 @@
+/** 
+ * @file	quadrado.cpp
+ * @brief	Implementacao da classe Quadrado para representar
+ *			um quadrado
+ * @author	Lael Rodrigues(laelrodrigues7@gmail.com)
+ * @since	22/11/2017
+ * @date 	22/11/2017
+ */
+
+
+#include "quadrado.h"
+
+#include <cmath>
+
+#include <iostream>
+using std::endl;
+using std::cout;
+
+#include <cstdlib>
+using std::exit;
+
+namespace geometria {
+
+	//Construtor padrao
+	Quadrado::Quadrado(){}
+
+	/** @param _lado Lado do quadrado */
+	Quadrado::Quadrado(float _lado){
+		lado = _lado;
+	}
+
+	/** @return Lado do quadrado */
+	float Quadrado::getLado() {
+		return lado;
+	}
+
+	/** @param _lado Lado do quadrado */
+	void Quadrado::setLado(float _lado) {
+		if(_lado <= 0 ) {
+			cout << "Valor invalido." << endl;
+			cout << "Programa finalizado." << endl;
+			exit(EXIT_FAILURE);
+		}
+		lado = _lado;
+	}
+
+	/** @return Area do Quadrado */
+	float Quadrado::calcArea() {
+		return lado * lado;
+	}
+
+	/** @return Perimetro do triangulo */
+	float Quadrado::calcPerimetro() {
+		return 4.0 * lado;
+	}
+
+	/**
+ 	 * @param o Referencia para stream de saida
+ 	 * @return Referencia para Stream de saida
+ 	 */
+	std::ostream& Quadrado::print(std::ostream &o) const {
+		o << "Lado do quadrado = " << lado;
+		return o;
+	}
+}
